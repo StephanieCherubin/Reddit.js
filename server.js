d9d14977dd2cb00ac0b90a02c16d7dbb15f224c1
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-12-12 13:58:33 -0800

    edit issue with bodyParser

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const db = require('./data/reddit-db.js');
 
 require('dotenv').config({
   path: path.join(__dirname, '.env')
@@ -23,6 +24,10 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(__dirname + '/public'));
 app.use(cookieParser()); // Add this after you initialize express.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
 var checkAuth = (req, res, next) => {
   console.log("Checking authentication");
@@ -35,8 +40,8 @@ var checkAuth = (req, res, next) => {
   }
   next();
 };
+
 app.use(checkAuth);
-const db = require('./data/reddit-db.js');
 
 const Posts = require('./controllers/posts.js')(app);
 const Post = require('./models/post.js');
@@ -45,25 +50,26 @@ const Auth = require('./controllers/auth.js')(app);
 const User = require('./models/user');
 
 //INDEX
-app.get('/', (req, res) => {
-  var currentUser = req.user;
-  Post.find({})
-    .then((posts) => {
-      res.render('post-index.handlebars', { 'posts': posts, 'currentUser': currentUser })
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-});
-
-app.listen(3000, function() {
-    console.log(new Date().toISOString() + ": Reddit Server started on port 3000");
-});
+// app.get('/', (req, res) => {
+//   // var currentUser = req.user;
+//   Post.find({})
+//     .then((posts) => {
+//       res.render('post-index.handlebars', { 'posts': posts, currentUser: req.user })
+//     })
+//     .catch((err) => {
+//       console.log(err.message);
+//     });
+// });
 
 if (require.main === module) {
     let port = process.env.PORT || 3000;
 
     app.listen(port, () => {
-        console.log(new Date().toISOString() + `App listening on port ${port}!`);
+        console.log(new Date().toISOString() + `Reddit server started on port ${port}!`);
     });
+}
+
+module.exports = app;
+module.exports.stop = () => {
+    return db.close()
 }
